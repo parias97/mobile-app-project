7a04525e5bbe1e769eddd87705895bdfd19201da
Peter Arias <peter.arias97@gmail.com>
2020-11-04 18:32:50 -0500

    Setup bottom navigation and up action button on action bar
    Properly setup bottom navigation to navigate to different fragments. The non top-level fragments display an Up action button on the action bar to navigate to top-level fragment.

--- app/src/main/java/com/example/flipper_app/MainActivity.java
+++ app/src/main/java/com/example/flipper_app/MainActivity.java
@@ -1,69 +1,46 @@
 package com.example.flipper_app;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
 
-import com.example.flipper_app.ui.addItem.AddItemFragment;
-import com.example.flipper_app.ui.favorites.FavoritesFragment;
-import com.example.flipper_app.ui.inventory.InventoryFragment;
-import com.example.flipper_app.ui.search.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity implements
-        BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
+
+    AppBarConfiguration appBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        navView.setOnNavigationItemSelectedListener(this);
 
-        // Load the inventory fragment. This will be the default fragment.
-        loadFragment(new InventoryFragment());
-
-        /* Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_search, R.id.navigation_add_item, R.id.navigation_favorites)
-                .build();
+        /**
+         * Find the navController (controls which views to display in NavHost) within NavHost
+         * (fragment container).
+         */
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        /**
+         * Instantiate appBarConfiguration to define which fragments are the top-level fragments.
+         * This will allow us to go up when viewing non-top-level fragments.
+         */
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);*/
-    }
-
-    private boolean loadFragment(Fragment fragment) {
-        if(fragment != null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, fragment)
-                    .commit();
-            return true;
-        }
-        return false;
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        Fragment fragment = null;
-        switch(menuItem.getItemId()) {
-            case R.id.navigation_search:
-                fragment = new SearchFragment();
-                break;
-            case R.id.navigation_add_item:
-                fragment = new AddItemFragment();
-                break;
-            case R.id.navigation_favorites:
-                fragment = new FavoritesFragment();
-                break;
-        }
-        return loadFragment(fragment);
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        // Navigate up to the top-level destination
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
+
 }
