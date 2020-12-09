38bb891ffeccf56df04b917b5a9e5a0632b82079
Peter Arias <peter.arias97@gmail.com>
2020-12-09 13:22:57 -0500

    Added the ability for the icons in the bottom navigation bar to change color when selected.

--- app/src/main/java/com/example/flipper_app/MainActivity.java
+++ app/src/main/java/com/example/flipper_app/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.flipper_app;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.flipper_app.ui.AddToSoldFragment;
 import com.example.flipper_app.ui.inventory.InventoryFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.flipper_app.model.Item;
 import androidx.fragment.app.Fragment;
@@ -18,10 +22,12 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager.widget.ViewPager;
 import com.example.flipper_app.adapter.ViewPagerAdapter;
+import com.google.android.material.internal.NavigationMenu;
 
 public class MainActivity extends AppCompatActivity {
 
     AppBarConfiguration appBarConfiguration;
+    BottomNavigationView navView;
     ViewPager viewPager;
     ViewPagerAdapter viewPagerAdapter;
 
@@ -29,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
 
         /**
          * Find the navController (controls which views to display in NavHost) within NavHost
@@ -48,8 +54,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        clearSelectedMenuItems(navView);
         // Navigate up to the top-level destination
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void clearSelectedMenuItems(BottomNavigationView navView){
+        Menu menu = navView.getMenu();
+        MenuItem item;
+        int menuItems = menu.size();
+
+        Log.d("clearing", "true");
+
+        for(int i = 0; i < menuItems; i++){
+            item = menu.getItem(i);
+            if(item.isChecked()) {
+                item.setChecked(false);
+            }
+        }
+
     }
 }
